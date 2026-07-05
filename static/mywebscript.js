13e26354fa4844a88d60ddc4a01110cf713055a7
tmchaudhary2005-design <tmchaudhary2005@gmail.com>
2026-07-05 21:58:29 +0530

    Update mywebscript.js

--- static/mywebscript.js
+++ static/mywebscript.js
@@ -1,1 +1,17 @@
-
+let RunEmotionDetection = () => {
+    // Get the text from the HTML text box
+    let textToAnalyze = document.getElementById("textToAnalyze").value;
+    
+    // Create an AJAX request to communicate with the Flask backend
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // Display the text returned by server.py
+            document.getElementById("system_response").innerHTML = xhttp.responseText;
+        }
+    };
+    
+    // Send the text to the /emotionDetector route
+    xhttp.open("GET", "/emotionDetector?textToAnalyze=" + encodeURIComponent(textToAnalyze), true);
+    xhttp.send();
+}
